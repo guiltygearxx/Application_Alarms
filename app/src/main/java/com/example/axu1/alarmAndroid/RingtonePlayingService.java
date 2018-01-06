36e9cdb58702aa4b75e9c9c1267f0ddd46ddf54a
nguyenvannam19942802 <nguyenvannam19942802@gmail.com>
2018-01-06 14:07:28 +0700

    #Add new  - Hoàn tất ứng dụng báo thức basic

---
+++ app/src/main/java/com/example/axu1/alarmAndroid/RingtonePlayingService.java
@@ -0,0 +1,124 @@
+package com.example.axu1.alarmAndroid;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+
+import java.util.Random;
+
+public class RingtonePlayingService extends Service {
+
+    private boolean isRunning;
+    private Context context;
+    MediaPlayer mMediaPlayer;
+    private int startId;
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.e("MyActivity", "In the Android service");
+        return null;
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+
+        final NotificationManager mNM = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
+        Intent intent1 = new Intent(this.getApplicationContext(), MainActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent1, 0);
+
+        Notification mNotify  = new Notification.Builder(this)
+                .setContentTitle("Alarm Android" + "!")
+                .setContentText("Click!")
+                .setSmallIcon(R.drawable.alarm)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true)
+                .build();
+
+        String state = intent.getExtras().getString("extra");
+
+        Log.e("what is going on here  ", state);
+
+        assert state != null;
+        switch (state) {
+            case "no":
+                startId = 0;
+                break;
+            case "yes":
+                startId = 1;
+                break;
+            default:
+                startId = 0;
+                break;
+        }
+
+
+        if(!this.isRunning) {
+            Log.e("if there was not sound ", " and you want start");
+
+            int min = 1;
+            int max = 9;
+
+            Random r = new Random();
+            int random_number = r.nextInt(max - min + 1) + min;
+            Log.e("random number is ", String.valueOf(random_number));
+
+
+                mMediaPlayer = MediaPlayer.create(this, R.raw.electronic1);
+            mMediaPlayer.start();
+
+
+            mNM.notify(0, mNotify);
+
+            this.isRunning = true;
+            this.startId = 0;
+
+        }
+        else {
+            Log.e("if there is sound ", " and you want end");
+
+            mMediaPlayer.stop();
+            mMediaPlayer.reset();
+
+            this.isRunning = false;
+            this.startId = 0;
+        }
+
+
+        Log.e("MyActivity", "In the service");
+
+        return START_NOT_STICKY;
+
+    }
+
+
+    @Override
+    public void onDestroy() {
+        Log.e("JSLog", "on destroy called");
+        super.onDestroy();
+
+        this.isRunning = false;
+    }
+
+
+
+
+
+
+
+}
